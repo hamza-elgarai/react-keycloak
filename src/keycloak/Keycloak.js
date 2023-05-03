74c11ab19aedc6140d9a87b466d131f538a7652f
hamdanimed <hamdanimee@gmail.com>
2023-05-03 15:53:08 +0100

    changes to keycloak configuration

--- src/keycloak/Keycloak.js
+++ src/keycloak/Keycloak.js
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 //This is the keycloak configuration file
 let keycloak = new Keycloak({
-    url: "http://localhost:8080",
-    realm: "pfsrealm",
+    url: "http://localhost:8081",
+    realm: "PFS",
     clientId: "react-app",
 })
 export default keycloak;
